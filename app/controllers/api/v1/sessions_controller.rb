53ccc84c0968cd5ec972c30e346c2401a4262343
Alec Wells <alec.wells47@gmail.com>
2020-03-26 13:39:53 -0600

    Add destroy action to session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -30,6 +30,14 @@ class SessionsController < ApplicationController
       }
     end
   end
+
+  def destroy
+    logout!
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
   # def create
   #   user = User
   #           .find_by(email: params["user"]["email"])
