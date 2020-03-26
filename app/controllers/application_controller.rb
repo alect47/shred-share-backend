f0a508694ad288c097272d752fe47fe36c1a9630
Alec Wells <alec.wells47@gmail.com>
2020-03-26 13:24:40 -0600

    Add heper method current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,7 @@ class ApplicationController < ActionController::Base
     !!session[:user_id]
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
