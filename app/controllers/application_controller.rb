c9dce63a9b2c855d6c6fc71e2183cb59abec8b72
Alec Wells <alec.wells47@gmail.com>
2020-03-26 13:25:12 -0600

    Add authorized_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authorized_user?
+     @user == current_user
+   end
 end
