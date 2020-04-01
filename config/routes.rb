0cb792ce9d30264d056feedc003b3c8e704ccf9d
Alec Wells <alec.wells47@gmail.com>
2020-04-01 12:32:54 -0600

    Add route for user deleting vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :vehicles, only: [:create, :index, :show]
 
   namespace :user do
-    resources :vehicles, only: [:index, :update]
+    resources :vehicles, only: [:index, :update, :destroy]
   end
 
 
