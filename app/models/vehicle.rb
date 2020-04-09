9447c361be987c667736057f42f792581493443e
Alec Wells <alec.wells47@gmail.com>
2020-04-09 12:35:02 -0600

    Add has_many relationship between vehicle and seat

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,8 @@
 class Vehicle < ApplicationRecord
 
   belongs_to :user
+  has_many :vehicles, dependent: :destroy
+  
   # has_many :seats, dependent: :destroy
-  validates_presence_of :make, :model 
+  validates_presence_of :make, :model
 end
