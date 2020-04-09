8c403358b4d3d5be99f7c2f7635448bd90c135b8
Alec Wells <alec.wells47@gmail.com>
2020-04-09 16:48:40 -0600

    Fix typo in trip model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,8 @@
 class Vehicle < ApplicationRecord
 
   belongs_to :user
-  has_many :vehicles, dependent: :destroy
-  
+  has_many :seats, dependent: :destroy
+
   # has_many :seats, dependent: :destroy
   validates_presence_of :make, :model
 end
