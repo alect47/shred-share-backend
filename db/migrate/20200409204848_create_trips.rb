887ad3c45105d562830523499b0cc8b5770ce1c4
Alec Wells <alec.wells47@gmail.com>
2020-04-09 14:53:10 -0600

    Add trip to database schema

--- db/migrate/20200409204848_create_trips.rb
+++ db/migrate/20200409204848_create_trips.rb
@@ -2,7 +2,7 @@ class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.string :origin
-      t.string :de stination
+      t.string :destination
       t.boolean :round_trip, default: true
 
       t.timestamps
