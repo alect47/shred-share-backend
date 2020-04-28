91c5235e6fdfeb2ac8c539be9f726546fc6b3d9e
Alec Wells <alec.wells47@gmail.com>
2020-04-28 14:01:11 -0600

    Add trip spec for post and get

---
+++ spec/requests/trip_request_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe "Vehicle endpoints" do
+  before(:each) do
+    @user = create(:user, email: 'user@email.com')
+    @user_2 = create(:user, email: 'user2@email.com')
+    @vehicle_1 = @user.vehicles.create(make: 'toyota', model: 'carola', id: 1)
+    @vehicle_2 = @user.vehicles.create(make: 'toyota', model: 'tacoma')
+    @vehicle_3 = @user_2.vehicles.create(make: 'toyota', model: 'tacoma', id: 3)
+    @trip_1 = @user_2.trips.create(origin: 'Denver, CO', destination: 'Vail, CO')
+    @trip_2 = @user_2.trips.create(origin: 'Denver, CO', destination: 'Aspen, CO')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  it 'user can POST trip' do
+
+    vehicle_info = '{"vehicle_id": "1", "num_seats": "3", "origin": "Denver, CO", "destination": "Vail, CO", "round_trip": "true"}'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    post '/user/trips', params: vehicle_info, headers: headers
+
+    expect(response).to be_successful
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to be_a Hash
+
+    # expect(results[:data][:attributes][:travel_time]).to eq("1 hour 48 mins")
+    #  Need to change response format to aligh with json
+    expect(results[:vehicle][:make]).to eq('Toyota')
+    expect(results[:vehicle][:model]).to eq('Tacoma')
+    expect(results[:vehicle][:year]).to eq('2007')
+    expect(results[:vehicle][:fourwd_or_awd]).to eq(false)
+    expect(results[:vehicle][:snow_tires]).to eq(false)
+
+  end
+
+  it 'user can GET their trips' do
+
+    get '/user/trips'
+
+    expect(response).to be_successful
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to be_a Hash
+    binding.pry
+
+    # expect(results[:data][:attributes][:travel_time]).to eq("1 hour 48 mins")
+    #  Need to change response format to aligh with json
+    expect(results[:vehicle][:make]).to eq('Toyota')
+    expect(results[:vehicle][:model]).to eq('Tacoma')
+    expect(results[:vehicle][:year]).to eq('2007')
+    expect(results[:vehicle][:fourwd_or_awd]).to eq(false)
+    expect(results[:vehicle][:snow_tires]).to eq(false)
+
+  end
+end
